41fc718c7a2fc7906deb4d87f733edd2676872d2
Drew Taphorn <84453016+dtap34@users.noreply.github.com>
2021-05-19 21:35:54 -0500

    Update Final_Project_Question_3_Part_4_12.08.2019.vb

--- Final_Project_Question_3_Part_4_12.08.2019.vb
+++ Final_Project_Question_3_Part_4_12.08.2019.vb
@@ -1,11 +1,11 @@
-﻿Public Class Form4
+﻿Public Class Final_Project_Question_3_Part_4_12.08.2019
     Private Sub Retrieve_Password_Click(sender As Object, e As EventArgs) Handles Retrieve_Password.Click
         Dim Username, SecurityAnswer As String
         Username = Username_Txt.Text
         SecurityAnswer = Security_Answer_Txt.Text
-        For i As Integer = 0 To Form2.Count - 1
-            If Username = Form2.Customers(i, 0) And SecurityAnswer = Form2.Customers(i, 4) Then
-                MsgBox("Your password is " & Form2.Customers(i, 1))
+        For i As Integer = 0 To Final_Project_Question_3_Part_2_12.08.2019.Count - 1
+            If Username = Final_Project_Question_3_Part_2_12.08.2019.Customers(i, 0) And SecurityAnswer = Final_Project_Question_3_Part_2_12.08.2019.Customers(i, 4) Then
+                MsgBox("Your password is " & Final_Project_Question_3_Part_2_12.08.2019.Customers(i, 1))
             Else
                 MsgBox("Your answer to the Security Question is incorrect")
             End If
@@ -19,7 +19,7 @@
     End Sub
 
     Private Sub Home_Click(sender As Object, e As EventArgs) Handles Home.Click
-        Form1.Show()
+        Final_Project_Question_3_Part_1_12.08.2019.Show()
         Me.Hide()
 
         Username_Txt.Clear()
@@ -34,8 +34,8 @@
         Username = Username_Txt.Text
         'SecurityAnswer = Security_Answer_Txt.Text
         For i As Integer = 0 To Form2.Count - 1
-            If Username = Form2.Customers(i, 0) Then
-                Security_Question_Txt.Text = Form2.Customers(i, 3)
+            If Username = Final_Project_Question_3_Part_2_12.08.2019.Customers(i, 0) Then
+                Security_Question_Txt.Text = Final_Project_Question_3_Part_2_12.08.2019.Customers(i, 3)
                 Exit Sub
             End If
         Next
