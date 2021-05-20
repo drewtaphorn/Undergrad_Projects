664ec4afb7392b860c08f9f4e34523a69595f361
Drew Taphorn <84453016+dtap34@users.noreply.github.com>
2021-05-19 21:32:51 -0500

    Update Final_Project_Question_3_Part_1_12.08.2019.vb

--- Final_Project_Question_3_Part_1_12.08.2019.vb
+++ Final_Project_Question_3_Part_1_12.08.2019.vb
@@ -1,7 +1,7 @@
 Public Class Final_Project_Question_3_Part_1_12.08.2019
     Private Sub Register_Click(sender As Object, e As EventArgs) Handles Register.Click
 
-        Form2.Show()
+        Final_Project_Question_3_Part_2_12.08.2019.Show()
         Me.Hide()
 
     End Sub
@@ -9,7 +9,7 @@ Public Class Final_Project_Question_3_Part_1_12.08.2019
     Private Sub Login_Click(sender As Object, e As EventArgs) Handles Login.Click
 
 
-        Form3.Show()
+        Final_Project_Question_3_Part_3_12.08.2019.Show()
         Me.Hide()
 
     End Sub
