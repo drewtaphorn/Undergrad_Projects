b2124f3c1828ef11597763cf266908027829de79
Drew Taphorn <84453016+dtap34@users.noreply.github.com>
2021-05-19 21:28:54 -0500

    Update Final_Project_Question_3_Part_1_12.08.2019.vb

--- Final_Project_Question_3_Part_1_12.08.2019.vb
+++ Final_Project_Question_3_Part_1_12.08.2019.vb
@@ -1,4 +1,4 @@
-﻿Public Class Form1
+Public Class Final_Project_Question_3_Part_1_12.08.2019
     Private Sub Register_Click(sender As Object, e As EventArgs) Handles Register.Click
 
         Form2.Show()
